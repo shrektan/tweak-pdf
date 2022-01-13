50698d8dd9eda33957214a8db9c8d642845e06d9
shrektan <shrektan@126.com>
2022-01-13 22:12:56 +0800

    only find pdfs

--- add-head-to-pdf.R
+++ add-head-to-pdf.R
@@ -1,6 +1,6 @@
 input_folder <- "input"
 output_folder <- "output"
-pdfs <- list.files(input_folder)
+pdfs <- list.files(input_folder, pattern = "[.]pdf$")
 
 # must remove the spaces in files as it cause LaTeX errors
 lapply(pdfs, \(x) {
@@ -10,7 +10,7 @@ lapply(pdfs, \(x) {
   file.rename(file.path(input_folder, x), file.path(input_folder, sanity(x)))
 }) |> invisible()
 
-pdfs <- list.files(input_folder)
+pdfs <- list.files(input_folder, pattern = "[.]pdf$")
 titles <- gsub(".pdf", "", pdfs, fixed = TRUE) |> trimws()
 
 for (i in seq_along(pdfs)) {
